34e991c616a479f61da05496791db1b8a688a399
BippyMiester <admin@jdswebservice.com>
2025-12-23 04:02:48 -0500

    Updated Discord Link

--- resources/views/app/modules/calltoaction.blade.php
+++ resources/views/app/modules/calltoaction.blade.php
@@ -7,7 +7,7 @@ Start Call To Action
             <div class="col-md-12 text-center">
                 <h2>Join Hateocracy — The Tower's multi-guild community</h2>
                 <p>First multi-guild community with a progress tracker, custom Discord bots, active staff, and a relaxed, newcomer-first atmosphere.</p>
-                <a href="#" class="btn btn-main">Join our Discord</a>
+                <a href="https://discord.hateocracy.com/" class="btn btn-main">Join our Discord</a>
             </div>
         </div> 		<!-- End row -->
     </div>   	<!-- End container -->
